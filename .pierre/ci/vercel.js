50f8788049c25e2a7b8cc6b4ed986b9cf261ebba
Taurean Bryant <taurean@hey.com>
2025-02-03 18:46:04 -0800

    adding vercel pierre job

---
+++ .pierre/ci/vercel.js
@@ -0,0 +1,7 @@
+import { Job } from "@pierre/vercel";
+export const label = "Vercel";
+
+export default Job({
+    VERCEL_ORG_ID: "team_M6NdTcuS2FKBnY2IEPmDiIXZ",
+    VERCEL_PROJECT_ID: "prj_s4blVCEknURBBjV3xQn9EaPB4wGW",
+});
