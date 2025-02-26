0474c1354d1ab5fca2c0979931d5c145bb58f86c
Taurean Bryant <later@taurean.work>
2025-02-26 12:21:31 -0800

    changing CSS import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  adapter: vercel()
+    integrations: [react()],
+    output: "server",
+    adapter: vercel(),
 });
